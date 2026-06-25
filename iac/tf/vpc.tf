f570d0eaf465664842770d155fa91132096eb64b
RenzoCf <monasasa1172@gmail.com>
2026-06-24 23:35:12 -0500

    fix(iac): corregir subnet pública que asignaba IP pública automáticamente (CKV_AWS_130)

--- iac/tf/vpc.tf
+++ iac/tf/vpc.tf
@@ -37,7 +37,7 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.101.0/24"
   availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.project_name}-public-az1"
